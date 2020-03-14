236f54e06a955504226861e3bbd8a3d9c05d7365
Andrew <a.holsaeter@gmail.com>
2020-03-14 22:02:16 +0100

    LED class

---
+++ led.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numberi$
+
+class LED():
+ def __init__(self, pin):
+  GPIO.setup(pin, GPIO.OUT)
+  self.pin = pin
+  self.previous_state = GPIO.LOW
+
+ def toggle(self):
+  if self.previous_state == GPIO.LOW:
+   GPIO.output(self.pin, GPIO.HIGH)
+   self.previous_state = GPIO.HIGH
+  else:
+   GPIO.output(self.pin, GPIO.LOW)
+   self.previous_state = GPIO.LOW
--- testClass.py
+++ testClass.py
@@ -1,11 +1,18 @@
 import time
 import RPi.GPIO as GPIO
 from button import Button
+from led import LED
 
 def printState(btn):
  print(btn.button_presses)
 
+def toggleLED(led):
+ led.toggle()
+
 btn = Button(29)
+led = LED(11)
+
+btn.subscribe(toggleLED, led)
 btn.subscribe(printState, btn)
 
 while(True):
